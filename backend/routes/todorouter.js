3d9335f4c44d952d35f93ffc64ef26eed5d93287
zohaib6262 <zohaibbinarhraaf@gmail.com>
2025-11-09 15:13:38 +0500

    Fix authMiddleware path in routes

--- backend/routes/todorouter.js
+++ backend/routes/todorouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express();
-// const { authenticate } = require("../middleware/authMiddleware");
-const authenticate = require("../middleware/authMiddleware");
+const { authenticate } = require("../middleware/authMiddleware");
 const TodoController = require("../controllers/todoController");
 
 router.post("/create-todo", authenticate, TodoController.createTodo);
